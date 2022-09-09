8517a13206f840778d2968bc621a7e1bb56b9c2e
Kartheek Chilvery <chilvery.kartheek@gmail.com>
2022-09-09 15:08:32 +0530

    Included Angular Material and upgraded Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material/material.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
