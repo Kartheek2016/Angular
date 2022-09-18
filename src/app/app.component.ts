b0203b1cad7f9ac33ec57caeec1e206378e75e06
Kartheek Chilvery <chilvery.kartheek@gmail.com>
2022-09-18 11:38:32 +0530

    Resolved an issues in menu reuting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,32 +3,31 @@ import { routes } from './app-routing.module';
 import { Route, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public showSubMenu: string = "";
+	public showSubMenu: string = "";
 
-  public menuItems: any[] = [];
+	public menuItems: any[] = [];
 
-  constructor(private _router: Router) { }
+	constructor(private _router: Router) { }
 
-  ngOnInit() {
-    this.menuItems = routes.filter(route => route.data && route.data['isMenu']);
-  }
+	ngOnInit() {
+		this.menuItems = routes.filter(route => route.data && route.data['isMenu']);
+	}
 
-  public navigate(menuItem: Route, isParent: boolean, childIndex: number): void {
-    if (menuItem.data && menuItem.data['isMenu']) {
-      if (isParent) {
-        if (menuItem.data['hasChildren']) {
-          this.showSubMenu = (this.showSubMenu && this.showSubMenu === menuItem.data['key']) ? "" : menuItem.data['key'];
-        } else {
-          this._router.navigateByUrl('/' + menuItem.path);
-        }
-      } else if (!isParent && menuItem.data['hasChildren']) {
-        this._router.navigateByUrl('/' + menuItem.path + '?scrollTo=' + menuItem.data['children'][childIndex].key);
-      }
-    }
-  }
+	public navigate(menuItem: Route, isParent: boolean, childIndex: number): void {
+		if (menuItem.data && menuItem.data['isMenu']) {
+			if (isParent) {
+				if (menuItem.data['hasChildren']) {
+					this.showSubMenu = (this.showSubMenu && this.showSubMenu === menuItem.data['key']) ? "" : menuItem.data['key'];
+				}
+				this._router.navigateByUrl('/' + menuItem.path);
+			} else if (!isParent && menuItem.data['hasChildren']) {
+				this._router.navigateByUrl('/' + menuItem.path + '?scrollTo=' + menuItem.data['children'][childIndex].key);
+			}
+		}
+	}
 }
